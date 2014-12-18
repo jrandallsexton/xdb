9cece4bf6161cb43a5b9f81a637bb6513425c580
Randall Sexton <jrandallsexton@gmail.com>
2014-12-18 18:53:57 -0500

    Initial commit

---
+++ XDB.Common/Enumerations/ECommonObjectType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XDB.Common.Enumerations
+{
+
+    public enum ECommonObjectType
+    {
+        XObject = 0,
+        XObjectType = 1,
+        XUser = 2,
+        XList = 3,
+        XListValue = 4,
+        XProperty = 5,
+        XValue = 6,
+        XRole = 7
+    }
+
+}
